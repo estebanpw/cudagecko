5f2e8e953fe8d839971442c783c7abb5fd3847be
Esteban <estebanpw@uma.es>
2020-04-08 09:20:06 +0200

    forgot to remove full seq print for debug

--- cuda/gpu_cuda_workflow.cu
+++ cuda/gpu_cuda_workflow.cu
@@ -276,9 +276,9 @@ int main(int argc, char ** argv)
     fprintf(stdout, "\t(End   rev)%.32s\n", &ref_rev_seq_host[ref_len-32]);
    
      
-    fprintf(stdout, "\t(Full que)%.*s\n", query_len, query_seq_host);
-    fprintf(stdout, "\t(Full ref)%.*s\n", ref_len, ref_seq_host);
-    fprintf(stdout, "\t(Full rev)%.*s\n", ref_len, ref_rev_seq_host);
+    //fprintf(stdout, "\t(Full que)%.*s\n", query_len, query_seq_host);
+    //fprintf(stdout, "\t(Full ref)%.*s\n", ref_len, ref_seq_host);
+    //fprintf(stdout, "\t(Full rev)%.*s\n", ref_len, ref_rev_seq_host);
 
 
     // Write header to CSV
